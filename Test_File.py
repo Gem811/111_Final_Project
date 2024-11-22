d79fdb4d7849c4eea749f2f9c6560b3356930ceb
Gem811 <166326098+Gem811@users.noreply.github.com>
2024-11-21 16:27:04 -0800

    Added header template

--- Test_File.py
+++ Test_File.py
@@ -1,1 +1,7 @@
+# Team member names
+# Date
+# CptS 111, Fall 2024
+# Name of project
+# Brief description and/or purpose of project; include sources
+# Modules used
 print(f'You are going to die in 7 days.') #well thats a bit dark
